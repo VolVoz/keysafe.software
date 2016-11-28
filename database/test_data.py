d2791e3a177a2ab39df298d4564269e1737cfb16
Ivan Konstantinovskyi <konstantinovskyi.ivan@gmail.com>
2016-11-28 15:46:40 +0200

    key pathes. add all keys pathes to his place.

--- database/test_data.py
+++ database/test_data.py
@@ -2,7 +2,6 @@
 
 from models import User, Key, UserKeyLink, KeyPlaces
 
-
 # users = [{"firstname": u"ПЕТРО", "lastname": u"ІВАНЕНКО", "rfid_card": "11111", "admin": True},
 #          {"firstname": u"СИДІР", "lastname": u"БЕРЕЗА", "rfid_card": "22222", "admin": False},
 #          {"firstname": u"ІВАН", "lastname": u"ІВАНЕНКО", "rfid_card": "33333", "admin": False},
@@ -14,11 +13,94 @@ from models import User, Key, UserKeyLink, KeyPlaces
 #         {"room": "212", "rfid_chip": "222"},
 #         {"room": "555", "rfid_chip": "555"},
 #         {"room": "343", "rfid_chip": "333"}]
-keys_places = [{"coordinate_place": "11111"},
-               {"coordinate_place": "22222"},
-               {"coordinate_place": "33333"},
-               {"coordinate_place": "44444"},
-               {"coordinate_place": "55555"}]
+# left 1st row
+"""
+# pppath = "35011 15012 35011"
+# pppath = "35011 33012 35011"
+# pppath = "35011 33012 35001"
+# pppath = "35011 50012 35011"
+# pppath = "35011 50012 35001"
+# right 1strow
+# pppath = "35011 15002 35011"
+# pppath = "35011 33002 35011"
+# pppath = "35011 33002 35001"
+# pppath = "35011 50002 35011"
+# pppath = "35011 50002 35001"
+"""
+# left 2st row
+"""
+# pppath = "140011 15012 35011"
+# pppath = "140011 15012 35001"
+# pppath = "140011 33012 35011"
+# pppath = "140011 33012 35001"
+# pppath = "140011 50012 35011"
+# pppath = "140011 50012 35001"
+# right 2strow
+# pppath = "140011 15002 35011"
+# pppath = "140011 15002 35001"
+# pppath = "140011 33002 35011"
+# pppath = "140011 33002 35001"
+# pppath = "140011 50002 35011"
+# pppath = "140011 50002 35001"
+"""
+#  left 3st row
+"""
+    # pppath = "245011 15012 35011"
+    # pppath = "245011 15012 35001"
+    # pppath = "245011 33012 35011"
+    # pppath = "245011 33012 35001"
+    # pppath = "245011 50012 35011"
+    # pppath = "245011 50012 35001"
+    # right 3strow
+    # pppath = "245011 15002 35011"
+    # pppath = "245011 15002 35001"
+    # pppath = "245011 33002 35011"
+    # pppath = "245011 33002 35001"
+    # pppath = "245011 50002 35011"
+    # pppath = "245011 50002 35001"
+"""
+
+keys_places = [
+    {"coordinate_place": "35011 15012 35011"},
+    {"coordinate_place": "35011 33012 35011"},
+    {"coordinate_place": "35011 33012 35001"},
+    {"coordinate_place": "35011 50012 35011"},
+    {"coordinate_place": "35011 50012 35001"},
+
+    {"coordinate_place": "35011 15002 35011"},
+    {"coordinate_place": "35011 33002 35011"},
+    {"coordinate_place": "35011 33002 35001"},
+    {"coordinate_place": "35011 50002 35011"},
+    {"coordinate_place": "35011 50002 35001"},
+
+    {"coordinate_place": "140011 15012 35011"},
+    {"coordinate_place": "140011 15012 35001"},
+    {"coordinate_place": "140011 33012 35011"},
+    {"coordinate_place": "140011 33012 35001"},
+    {"coordinate_place": "140011 50012 35011"},
+    {"coordinate_place": "140011 50012 35001"},
+
+    {"coordinate_place": "140011 15002 35011"},
+    {"coordinate_place": "140011 15002 35001"},
+    {"coordinate_place": "140011 33002 35011"},
+    {"coordinate_place": "140011 33002 35001"},
+    {"coordinate_place": "140011 50002 35011"},
+    {"coordinate_place": "140011 50002 35001"},
+
+    {"coordinate_place": "245011 15012 35011"},
+    {"coordinate_place": "245011 15012 35001"},
+    {"coordinate_place": "245011 33012 35011"},
+    {"coordinate_place": "245011 33012 35001"},
+    {"coordinate_place": "245011 50012 35011"},
+    {"coordinate_place": "245011 50012 35001"},
+
+    {"coordinate_place": "245011 15002 35011"},
+    {"coordinate_place": "245011 15002 35001"},
+    {"coordinate_place": "245011 33002 35011"},
+    {"coordinate_place": "245011 33002 35001"},
+    {"coordinate_place": "245011 50002 35011"},
+    {"coordinate_place": "245011 50002 35001"},
+]
 
 
 def create_test_data():
@@ -28,37 +110,37 @@ def create_test_data():
             print 'Places created!'
     except Exception as e:
         print 'Error - {}'.format(e)
-    # try:
-    #     for user in users:
-    #         User.create(user['firstname'], user['lastname'], user['rfid_card'], user['admin'])
-    #         print 'Users created!'
-    # except Exception as e:
-    #     print 'Error - {}'.format(e)
-    # try:
-    #     for key in keys:
-    #         Key.create(key['room'], key['rfid_chip'])
-    #     print 'Keys added!'
-    # except Exception as e:
-    #     print 'Error - {}'.format(e)
-    #
-    # try:
-    #     user1 = User.get_by_rfid(users[0]['rfid_card'])['data']
-    #     user2 = User.get_by_rfid(users[1]['rfid_card'])['data']
-    #     user3 = User.get_by_rfid(users[2]['rfid_card'])['data']
-    #     key1 = Key.get_by_rfid(keys[0]['rfid_chip'])['data']
-    #     key2 = Key.get_by_rfid(keys[1]['rfid_chip'])['data']
-    #     key3 = Key.get_by_rfid(keys[2]['rfid_chip'])['data']
-    #     print 'Getting users from db!'
-    # except Exception as e:
-    #     print 'Error - {}'.format(e)
-    #
-    # try:
-    #     print "Start operations with keys"
-    #     UserKeyLink.getting_key(user1, key1.rfid_chip)['data']
-    #     UserKeyLink.getting_key(user2, key2.rfid_chip)['data']
-    #     UserKeyLink.getting_key(user3, key3.rfid_chip)['data']
-    #     UserKeyLink.returning_key(key2.rfid_chip)['data']
-    #     UserKeyLink.returning_key(key1.rfid_chip)['data']
-    #     print "Done"
-    # except Exception as e:
-    #     print 'Error - {}'.format(e)
+        # try:
+        #     for user in users:
+        #         User.create(user['firstname'], user['lastname'], user['rfid_card'], user['admin'])
+        #         print 'Users created!'
+        # except Exception as e:
+        #     print 'Error - {}'.format(e)
+        # try:
+        #     for key in keys:
+        #         Key.create(key['room'], key['rfid_chip'])
+        #     print 'Keys added!'
+        # except Exception as e:
+        #     print 'Error - {}'.format(e)
+        #
+        # try:
+        #     user1 = User.get_by_rfid(users[0]['rfid_card'])['data']
+        #     user2 = User.get_by_rfid(users[1]['rfid_card'])['data']
+        #     user3 = User.get_by_rfid(users[2]['rfid_card'])['data']
+        #     key1 = Key.get_by_rfid(keys[0]['rfid_chip'])['data']
+        #     key2 = Key.get_by_rfid(keys[1]['rfid_chip'])['data']
+        #     key3 = Key.get_by_rfid(keys[2]['rfid_chip'])['data']
+        #     print 'Getting users from db!'
+        # except Exception as e:
+        #     print 'Error - {}'.format(e)
+        #
+        # try:
+        #     print "Start operations with keys"
+        #     UserKeyLink.getting_key(user1, key1.rfid_chip)['data']
+        #     UserKeyLink.getting_key(user2, key2.rfid_chip)['data']
+        #     UserKeyLink.getting_key(user3, key3.rfid_chip)['data']
+        #     UserKeyLink.returning_key(key2.rfid_chip)['data']
+        #     UserKeyLink.returning_key(key1.rfid_chip)['data']
+        #     print "Done"
+        # except Exception as e:
+        #     print 'Error - {}'.format(e)
--- hardware/path.py
+++ hardware/path.py
@@ -8,8 +8,56 @@ if __name__ == '__main__':
     mtr1 = MotorDRV_a4988(motor1_step, motor1_dir, motors_enable)
     mtr2 = MotorDRV_a4988(motor2_step, motor2_dir, motors_enable)
     mgn = Magnet(magnet)
+    # left 1st row
+    """
+    # pppath = "35011 15012 35011"
+    # pppath = "35011 33012 35011"
+    # pppath = "35011 33012 35001"
+    # pppath = "35011 50012 35011"
+    # pppath = "35011 50012 35001"
+    # right 1strow
+    # pppath = "35011 15002 35011"
+    # pppath = "35011 33002 35011"
+    # pppath = "35011 33002 35001"
+    # pppath = "35011 50002 35011"
+    # pppath = "35011 50002 35001"
+    """
+    # left 2st row
+    """
+    # pppath = "140011 15012 35011"
+    # pppath = "140011 15012 35001"
+    # pppath = "140011 33012 35011"
+    # pppath = "140011 33012 35001"
+    # pppath = "140011 50012 35011"
+    # pppath = "140011 50012 35001"
+    # right 2strow
+    # pppath = "140011 15002 35011"
+    # pppath = "140011 15002 35001"
+    # pppath = "140011 33002 35011"
+    # pppath = "140011 33002 35001"
+    # pppath = "140011 50002 35011"
+    # pppath = "140011 50002 35001"
+    """
+    #  left 3st row
+    """
+    # pppath = "245011 15012 35011"
+    # pppath = "245011 15012 35001"
+    # pppath = "245011 33012 35011"
+    # pppath = "245011 33012 35001"
+    # pppath = "245011 50012 35011"
+    # pppath = "245011 50012 35001"
+    # right 3strow
+    # pppath = "245011 15002 35011"
+    # pppath = "245011 15002 35001"
+    # pppath = "245011 33002 35011"
+    # pppath = "245011 33002 35001"
+    # pppath = "245011 50002 35011"
+    # pppath = "245011 50002 35001"
+    """
+    pppath = "245011 50002 35001"
+    # mgn.up()
 
-    pppath = "50011 50012"
+    sleep(0.1)
     for comnd in pppath.split():
         motor = int(comnd[-1])
         direction = int(comnd[-2])
@@ -19,7 +67,9 @@ if __name__ == '__main__':
             mtr1.spin(steps, direction)  # 500 1 1
         if motor is 2:
             mtr2.spin(steps, direction)
-
+    # mgn.down()
+    mgn.up()
+    sleep(0.5)
     for comnd in pppath.split()[::-1]:
         print comnd
         motor = int(comnd[-1])
@@ -38,3 +88,4 @@ if __name__ == '__main__':
             # mtr2.spin(500,0)
             # mtr1.spin(500,0)
             # mgn.down() # 03
+    mgn.down() # 03
